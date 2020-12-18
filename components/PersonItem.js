f5242a7f4ce3a4818b3e5fe702bdd067da3ab135
RahmatMulyaSimanjuntak <rahmatsnsd@gmail.com>
2020-12-18 19:05:56 +0700

    First Commit

---
+++ components/PersonItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import  {
+	View
+} from 'react-native';
+
+
+import Icon from 'react-native-vector-icons/dist/Ionicons';
+import config from '../../config';
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Addproduct from './components/screen/addproduct';
+
+AppRegistry.registerComponent(appName, () => Addproduct);
